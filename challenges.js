13651edcd44c0d21689163687a14fef94d5bb101
Yassine Douhabi <douhabiyassine@gmail.com>
2023-12-11 17:53:21 +0100

    update

--- challenges.js
+++ challenges.js
@@ -1,19 +1,4 @@
-/*
-  Array Challenge
-*/
+let str = "hello";
+let stringReversal = str.split("").reverse().join("");
 
-let zero = 0;
-
-let counter = 3;
-
-let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
-
-// Write Code Here
-
-console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
-
-console.log(my.slice("????")); // ["Elham", "Mazero"]
-
-console.log(); // "Elzero"
-
-console.log(); // "rO"
+console.log(stringReversal);
